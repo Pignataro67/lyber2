5c5e77509097559ea22109a5ff1e6f09b4b0f2c3
Pignataro67 <Pignataro67@gmail.com>
2019-08-25 09:48:03 -0700

    adding redirrect funcly in confirmRoute comp

--- lyber2/client/components/confirmRoute/ConfirmRoute.js
+++ lyber2/client/components/confirmRoute/ConfirmRoute.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import Button from '../Button';
 import RouteMap from './RouteMap';
 import Card from '../Card';
+import { Redirect } from "react-router-dom";
 
- class ConfirmRoute extends Component{
+class ConfirmRoute extends Component{
   state = {
     redirectToResults: false,
     redirectToSearch: false
   }
 
-    render(){
+  handleRedirect = async (e) => {
+    e.preventDefault()
+    await this.props.actions.fetchUberEstimate(this.props.startLat, this.props.startLng, this.props.destinationLat, this.props.destinationLng)
+    await this.props.actions.fetchLyftEstimate(this.props.startLat, this.props.startLng, this.props.destinationLat, this.props.destinationLng)
+    this.setState({
+      redirectToResults: true
+    })
+  }
+
+    handleRedirectBack = () => {
+    this.setState({
+      redirectToSearch: true
+    })
+  }
+
+  render(){
   const { redirectToResults } = this.state;
   const { redirectToSearch } = this.state;
     if(redirectToResults) {
@@ -21,10 +37,12 @@ import Card from '../Card';
     <div>
       <Card>
         <RouteMap mapboxKey={this.props.mapboxKey} startLat={this.props.startLat} startLng={this.props.startLng} destinationLat={this.props.destinationLat} destinationLng={this.props.destinationLng}/>
+        <Button buttonTitle="No, let's try that again." onClick={this.handleRedirectBack}/> 
+        <Button buttonTitle="Yes! Let's Ride..." onClick={this.handleRedirect}/>
       </Card>
     </div>
     )
   }
 }
 
- export default ConfirmRoute;
+export default ConfirmRoute;
