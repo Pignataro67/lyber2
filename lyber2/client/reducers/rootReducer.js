aa87776cd2168a6133c47229f516cedd311bc692
Pignataro67 <Pignataro67@gmail.com>
2019-08-26 10:11:29 -0700

    adding reducers

---
+++ lyber2/client/reducers/rootReducer.js
@@ -0,0 +1,39 @@
+export default function rootReducer(state = {
+  isFetchingMapboxKey: false,
+  mapBoxKey: '',
+  isFetchingStartingLocation: false,
+  isFetchingDropOff: false,
+  suggestedPickupLocations: [],
+  suggestedDropOff: [],
+  isConvertingPickupLatLong: false,
+  isConvertingDropoffLatLong: false,
+  pickupLat: '',
+  pickupLong: '',
+  dropoffLat: '',
+  dropoffLong: '',
+    }, action) {
+    switch(action.type){
+      case "FETCHING_MAPBOX_KEY":
+        return {...state, isFetchingMapboxKey: true}
+      case "ADD_MAPBOX_KEY_TO_STATE":
+        return {...state, isFetchingMapboxKey: false, mapBoxKey: action.key}
+      case "FETCHING_SUGGESTED_PICKUP_LOCATIONS":
+        return {...state, isFetchingPickupLocation: true, suggestedPickupLocations: []}
+      case "DISPLAY_PICKUP_LOCATIONS":
+        return {...state, isFetchingPickupLocation: false, suggestedPickupLocations: action.suggestedPickupLocations}
+      case "FETCHING_SUGGESTED_DROPOFFS": 
+        return {...state, isFetchingDropOff: true, suggestedDropOffs: []}
+      case "DISPLAY_DROPOFFS":
+        return {...state, isFetchingDropOff: false, suggestedDropOffs: action.suggestedDropOffs}
+      case "CONVERTING_PICKUP_LAT_LONG":
+        return {...state, isConvertingPickupLatLong: true}
+      case "RETRIEVE_PICKUP_LAT_LONG":
+        return {...state, isConvertingPickupLatLong: false, pickupLatLong: action.pickupLatLong}
+      case "CONVERTING_DROPOFF_LAT_LONG":
+        return {...state, isConvertingDropoffLatLong: true}
+      case "RETRIEVE_DROPOFF_LAT_LONG":
+        return {...state, isConvertingDropoffLatLong: false, dropoffLatLong: action.dropoffLatLong}  
+      default: 
+        return state
+    }
+}
