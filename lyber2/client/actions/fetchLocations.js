3974c9541160059f9c85bd9c83a0c1e7ab29f66d
Pignataro67 <Pignataro67@gmail.com>
2019-08-27 13:40:56 -0700

    adding actions

--- lyber2/client/actions/fetchLocations.js
+++ lyber2/client/actions/fetchLocations.js
@@ -23,3 +23,29 @@ function _convertDropoffLatLong(location) {
     return _getLatLong(location).then(({ lat, long }) => dispatch({type: 'RETRIEVE_DROPOFF_LAT_LONG', dropoffLat: lat, dropoffLong: long}))
   }
 }
+
+export function fetchPickupLocation(input) {
+  console.log(input)
+    return (dispatch) => {
+      dispatch({ type: 'FETCHING_SUGGESTED_PICKUP_LOCATIONS' });
+      _fetchLocations(input).then(suggestedPickupLocations => 
+      dispatch({ type: 'DISPLAY_PICKUP_LOCATIONS', suggestedPickupLocations }));
+  };
+}
+  
+export function fetchDropOff(input) {
+  console.log(input)
+    return (dispatch) => {
+      dispatch({ type: 'FETCHING_SUGGESTED_DROPOFFS' });
+      _fetchLocation(input).then(suggestedDropOffs => 
+      dispatch({ type: 'DISPLAY_DROPOFFS', suggestedDropoffs }));
+  };
+}
+  
+export function convertLatLong(pickupLocation, dropoffLocation) {
+  console.log(location)
+  return async (dispatch) => {
+    await dispatch(_convertPickupLatLong(pickupLocation))
+    await dispatch(_convertDropoffLatLong(dropoffLocation))
+  }
+}
