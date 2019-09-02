154fcf6b25f08a60fea7a30fa6ee9c07fe4cb89a
Pignataro67 <Pignataro67@gmail.com>
2019-09-02 07:46:28 -0700

    even mo' stylin', adding ms2p & md2p in App.js

--- lyber2/client/containers/resultsContainer.rb
+++ lyber2/client/containers/resultsContainer.rb
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Results from '../components/results/Results';
 import CardLabel from '../components/CardLabel';
-import card from '../components/Card'
+import card from '../components/Card';
 
   class ResultsContainer extends Component {
     render() {
+        const cardStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        WebkitTransition: 'all',
+        msTransition: 'all'
+      };
       let cardlabel = "shutup and drive"
 
       return (
-        <Card>
+        <Card style={cardStyle}>
           <CardLabel cardLabel={cardLabel} />
           <Results />
         </Card>
