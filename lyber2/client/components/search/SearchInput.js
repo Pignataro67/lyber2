5c2415a18faec8be243ddb20f09fbbe625e197e0
Pignataro67 <Pignataro67@gmail.com>
2019-09-03 15:10:43 -0700

    streamlining functionality

--- lyber2/client/components/search/SearchInput.js
+++ lyber2/client/components/search/SearchInput.js
@@ -12,8 +12,8 @@ class SearchInput extends Component {
   }
 
 componentDidUpdate(prevProps) {
-  if(prevProps.suggestedLocations !==
-  this.props.suggestedLocations) {
+  if(prevProps.suggestedLocations &&
+    this.props.suggestedLocations.length) {
     this.addSuggestedLocationsToState(this.props.suggestedLocations)
     }
   }
@@ -39,7 +39,8 @@ onFocus = () => this.setState({ isDropDownOpen: true },
   render() {
     return (
       <form onSubmit={this.props.onSubmit}>
-        <Dropdown 
+        <Dropdown
+          loading={this.props.loading}
           onSearchChange={this.props.onChange}
           onChange={this.handleDropDownChange} 
           placeholder={this.props.label} 
