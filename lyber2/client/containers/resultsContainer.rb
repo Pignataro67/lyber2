5a5c76dd14590451fb3ce226dfbb03bc4469461c
Pignataro67 <Pignataro67@gmail.com>
2019-09-06 12:14:39 -0700

    adding map state and connect

--- lyber2/client/containers/resultsContainer.rb
+++ lyber2/client/containers/resultsContainer.rb
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import Results from '../components/results/Results';
 import CardLabel from '../components/CardLabel';
 import card from '../components/Card';
+import Card from '../components/Card';
+import { connect } from 'react-redux';
 
-  class ResultsContainer extends Component {
-    render() {
-        const cardStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        WebkitTransition: 'all',
-        msTransition: 'all'
-      };
-      let cardlabel = "shutup and drive"
+class ResultsContainer extends Component {
+  render() {
+    const cardStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    WebkitTransition: 'all',
+    msTransition: 'all'
+  };
+    let cardlabel = "shutup and drive"
 
-      return (
-        <Card style={cardStyle}>
-          <CardLabel cardLabel={cardLabel} />
-          <Results />
-        </Card>
-      )
-    }
+    return (
+      <Card style={cardStyle}>
+        <CardLabel cardLabel={cardLabel} />
+        <Results {...this.props}/>
+      </Card>
+    )
   }
+}
 
-export default ResultsContainer; 
+const mapStateToProps = state => ({
+  ...state
+})
+
+export default connect(mapStateToProps)(ResultsContainer); 
